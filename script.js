d2c47e27854a1298745237f6140e6c907cb54c29
HuartYonis <78868208+HuartYonis@users.noreply.github.com>
2024-04-08 13:29:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     imageContainers.forEach(container => {
         container.addEventListener("click", function () {
-            // Afficher le texte de l'image
+            // Retirer le texte de l'image précédente
+            const previousOverlay = document.querySelector(".overlay");
+            if (previousOverlay) {
+                previousOverlay.remove();
+            }
+
+            // Afficher le texte de l'image actuelle
             const text = this.querySelector("img").getAttribute("data-text");
             const overlay = document.createElement("div");
             overlay.classList.add("overlay");
             overlay.innerText = text;
             this.appendChild(overlay);
 
-            // Déplacer l'image cliquée au centre
+            // Centrer l'image cliquée
             const clickedImage = this.cloneNode(true);
             this.parentNode.insertBefore(clickedImage, this.parentNode.firstChild);
             this.remove();
 
-            // Déplacer les autres images en haut
+            // Centrer les autres images en haut
             const otherImages = document.querySelectorAll(".image-container");
             otherImages.forEach(image => {
                 if (image !== clickedImage) {
